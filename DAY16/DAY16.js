18f3ea69ed42c31fbc61a27f5fb0e5bfea847dff
RamyaSrimana <ramyasrimana@gmail.com>
2026-04-01 09:12:06 +0530

    Add files via upload

---
+++ DAY16/DAY16.js
@@ -0,0 +1,26 @@
+var name = "Ramya";
+let age = 22;
+const Course = "FullStack";
+
+let str = "Hello World";
+let isActive = true;
+let emptyValue = null;
+let not_Assigned;
+
+let a = 10;
+let b = 3;
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Modulus:", a % b);
+
+console.log("a == b:", a == b);
+console.log("a === b:", a === b);
+
+let x = true;
+let y = false;
+
+console.log("x && y:", x && y);
+console.log("x || y:", x || y);
